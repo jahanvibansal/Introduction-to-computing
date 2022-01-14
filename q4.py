4beedf4cc83d57b6c3c1780905fdca67ee627083
jahanvibansal <97628128+jahanvibansal@users.noreply.github.com>
2022-01-14 13:34:05 +0530

    Delete q4.py

--- q4.py
+++
@@ -1,8 +0,0 @@
-a=int(input("Marks of first student "))
-b=int(input("Marks of second student "))
-c=int(input("Marks of third student "))
-d=int(input("Marks of fourth student "))
-e=int(input("Marks of fifth student "))
-Marks=[a,b,c,d,e]
-Marks.sort()
-print(Marks)
